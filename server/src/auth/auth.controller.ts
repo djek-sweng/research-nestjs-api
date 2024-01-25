97a3c864db2d12553a63607683351b1d7a1ded3b
djek-sweng <djek@gmx.ch>
2024-01-25 22:15:11 +0100

    .

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
 
 @Controller({})
-class AuthController {}
+export class AuthController {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-class AuthService {}
+export class AuthService {}
