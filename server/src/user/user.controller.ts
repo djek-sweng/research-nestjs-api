47231024106fc5a104e1c15d56b55a302d0b741e
djek-sweng <djek@gmx.ch>
2024-01-28 06:58:35 +0100

    s

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -4,11 +4,6 @@ import { JwtAuthGuard } from 'src/auth/jwt';
 @UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UserController {
-  @Get('me')
-  getMe() {
-    return { message: 'Hello. It is me.' };
-  }
-
   @Get('profile')
   getProfile(@Request() req: any) {
     return req.user;
