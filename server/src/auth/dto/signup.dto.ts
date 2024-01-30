970891296fce62ce6f105e747b5631528974b340
djek-sweng <djek@gmx.ch>
2024-01-30 20:12:36 +0100

    s

--- server/src/auth/dto/signup.dto.ts
+++ server/src/auth/dto/signup.dto.ts
@@ -1,5 +1,5 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { SigninDto } from '.';
+import { SigninDto } from './signin.dto';
 
 export class SignupDto extends SigninDto {
   @IsString()
