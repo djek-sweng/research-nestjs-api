07687dea390761a5f8052fd12037384b947d3fd7
djek-sweng <djek@gmx.ch>
2024-01-30 21:25:42 +0100

    s

--- server/src/notebook/notebook.module.ts
+++ server/src/notebook/notebook.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { NotebookService } from './notebook.service';
+import { NotebookController } from './notebook.controller';
 
-@Module({})
+@Module({
+  providers: [NotebookService],
+  controllers: [NotebookController],
+})
 export class NotebookModule {}
