255996a92f53cc5e11549d3059ccb38fb4085258
djek-sweng <djek@gmx.ch>
2024-01-31 11:27:50 +0100

    s

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+const port = new ConfigService().getOrThrow('PORT');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(5000);
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => {
+    console.log(`Nest application listen on port ${port}.`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
