36c659c7d79a946cbefd46dcea922a0e474a19c6
djek-sweng <djek@gmx.ch>
2024-01-31 12:03:55 +0100

    s

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const port = new ConfigService().getOrThrow('PORT');
@@ -8,7 +9,16 @@ const port = new ConfigService().getOrThrow('PORT');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(helmet());
 
   await app.listen(port);
 }
